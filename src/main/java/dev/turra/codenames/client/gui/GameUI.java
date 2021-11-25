02b967ba9c078e8643eb7252232dc336fdee8fba
Turrra <artikl30112000@gmail.com>
2021-11-24 20:53:15 -0500

    [+] Finalizing on a complete game

--- src/main/java/dev/turra/codenames/client/gui/GameUI.java
+++ src/main/java/dev/turra/codenames/client/gui/GameUI.java
@@ -1,20 +1,15 @@
 package dev.turra.codenames.client.gui;
 
 import dev.turra.codenames.client.mechanics.GameManager;
-import dev.turra.codenames.client.mechanics.Player;
 import dev.turra.codenames.common.Role;
 import dev.turra.codenames.common.Team;
 import dev.turra.codenames.common.network.sb.PacketServerCardClick;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 
-public class GameUI extends JFrame{
+public class GameUI extends JFrame {
 	private GameManager manager;
 
 	private CardLayout layout = new CardLayout();
@@ -73,21 +68,21 @@ public class GameUI extends JFrame{
 //		board.col
 	}
 
-	private void loginWindow(){
+	private void loginWindow() {
 		layout.show(mainPanel, "login");
 		connectButton.addActionListener(e -> {
 			try {
 				manager.connectionAttempt(inputUsername.getText(), inputIP.getText(), Integer.parseInt(inputPort.getText()));
 				gameWindow();
 				errorText.setText(" ");
-			}catch (IOException | NumberFormatException ex){
+			} catch (IOException | NumberFormatException ex) {
 				errorText.setText("Could not connect to the server.");
 				ex.printStackTrace();
 			}
 		});
 	}
 
-	private void gameWindow(){
+	private void gameWindow() {
 		boardPanel.setLayout(new GridLayout(5, 5, 2, 2));
 		layout.show(mainPanel, "game");
 
@@ -110,31 +105,31 @@ public class GameUI extends JFrame{
 		}
 	}
 
-	public void hideTeamButtons(){
+	public void hideTeamButtons() {
 		joinBlueOperativesButton.setVisible(false);
-        joinBlueSpymastersButton.setVisible(false);
-        joinRedOperativesButton.setVisible(false);
-        joinRedSpymastersButton.setVisible(false);
+		joinBlueSpymastersButton.setVisible(false);
+		joinRedOperativesButton.setVisible(false);
+		joinRedSpymastersButton.setVisible(false);
 	}
 
-	public void showRoleUI(Role role){
-		if(role == Role.SPYMASTER){
+	public void showRoleUI(Role role) {
+		if (role == Role.SPYMASTER) {
 			spymasterUI.setVisible(true);
-		}else{
+		} else {
 			operativeUI.setVisible(true);
 		}
 	}
 
-	public void updateCard(int x, int y, String word, Color color){
+	public void updateCard(int x, int y, String word, Color color) {
 		// debug
 		System.out.println("Updating card " + x + "x" + y + " to " + word);
 		board[x][y].setWord(word);
-		if(color != null)
-        	board[x][y].getWordLabel().setForeground(color);
+		if (color != null)
+			board[x][y].getWordLabel().setForeground(color);
 	}
 
-	public void cardClicked(int x, int y){
-		if(manager.getPlayer().getTeam() == null || manager.getPlayer().getRole() == null)
+	public void cardClicked(int x, int y) {
+		if (manager.getPlayer().getTeam() == null || manager.getPlayer().getRole() == null)
 			return;
 
 		manager.getPlayer().sendPacket(new PacketServerCardClick(x, y));
@@ -144,22 +139,22 @@ public class GameUI extends JFrame{
 		board[x][y].reveal(color);
 	}
 
-	public void setHint(String hint, int wordAmount){
+	public void setHint(String hint, int wordAmount) {
 		givenHint.setText(hint + " " + wordAmount);
 	}
 
-	public void clearHint(){
+	public void clearHint() {
 		givenHint.setText("");
 	}
 
-	public void setTurn(Team team){
+	public void setTurn(Team team) {
 		currentTurnLabel.setText("Current Turn: " + team.toString());
 		currentTurnLabel.setForeground(team.getColor().getColor());
 	}
 
-	public void setAnnouncerText(String text, Color color){
+	public void setAnnouncerText(String text, Color color) {
 		announcerText.setText(text);
-        announcerText.setForeground(color);
+		announcerText.setForeground(color);
 	}
 
 	public static void main(String[] args) {
